87c04aa8bff8ac73af13891c99d89b48deb7ce53
laiabelaustegui <laia.nuria.belaustegui@estudiantat.upc.edu>
2025-12-07 01:07:15 +0100

    allow cors

--- backend/settings.py
+++ backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'llms',
     'experiments',
     'prompts',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,7 +51,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'backend.urls'
 
